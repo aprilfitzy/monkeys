3122ca20d70588a1d414172990d245664701aebd
April Fitzpatrick <89559817+aprilfitzy@users.noreply.github.com>
2024-10-07 17:10:46 -0400

    test alert script

---
+++ wapo.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Hello World
+// @namespace   wapo
+// @include     https://www.washingtonpost.com/crossword-puzzles/daily/*
+// @version     1
+// ==/UserScript==
+
+alert("hello world");
